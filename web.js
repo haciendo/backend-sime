19e94f591940c0592aa98a5168b089cf7964afb2
Javier Lurgo (ntbk) <jlurgo@gmail.com>
2015-03-26 16:04:16 -0300

    idTipoPieza y idCota

--- web.js
+++ web.js
@@ -3,6 +3,7 @@ var Vx = Vortex.Vx;
 
 var server_vortex = new Vortex.ServerVortex();
 
+var _ = require("./underscore-min");
 var mongodb = require('mongodb');
 var uri = 'mongodb://admin:haciendo@ds033599.mongolab.com:33599/sime-backend';
 
@@ -50,6 +51,11 @@ mongodb.MongoClient.connect(uri, function(err, db) {
 		tipoDeMensaje: 'buscarTipoPiezas'
 	}, function(busq_piezas){
 		col_piezas.find({}).toArray(function(err, piezas){
+			piezas = _.map(piezas, function(pieza){ 
+				pieza["idTipoPieza"] = pieza["_id"];
+        		delete pieza["_id"];
+				return pieza;
+			});
 			Vx.send({
 				tipoDeMensaje: "Vortex.respuesta",
 				responseTo: busq_piezas.idRequest,
@@ -63,6 +69,11 @@ mongodb.MongoClient.connect(uri, function(err, db) {
 		tipoDeMensaje: 'buscarCotas'
 	}, function(busq_cotas){
 		col_cotas.find({idTipoPieza:busq_cotas.idTipoPieza}).toArray(function(err, cotas){
+			cotas = _.map(cotas, function(cota){ 
+				cota["idCota"] = cota["_id"];
+				delete cota["_id"];
+				return cota;
+			});
 			Vx.send({
 				tipoDeMensaje: "Vortex.respuesta",
                 responseTo: busq_cotas.idRequest,
