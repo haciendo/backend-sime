dd07ecd545362c3ebd2ae464a1e7750fc0acf5a8
Javier Lurgo (ntbk) <jlurgo@gmail.com>
2015-04-21 10:06:58 -0300

    adaptacion a documentos

--- web.js
+++ web.js
@@ -5,13 +5,15 @@ var server_vortex = new Vortex.ServerVortex();
 
 var _ = require("./underscore-min");
 var mongodb = require('mongodb');
-var uri = 'mongodb://admin:haciendo@ds033599.mongolab.com:33599/sime-backend';
+var uri = 'mongodb://127.0.0.1/Sime';
+//var uri = 'mongodb://admin:haciendo@ds033599.mongolab.com:33599/sime-backend';
 
 mongodb.MongoClient.connect(uri, function(err, db) {  
   	if(err) throw err;
 	var col_usuarios = db.collection('usuarios');
-	var col_piezas = db.collection('tipoPiezas');
-	var col_cotas = db.collection('cotas');
+	var col_instrumentos = db.collection('instrumentos');
+	var col_tipos_de_pieza = db.collection('tiposDePieza');
+	var col_piezas = db.collection('piezas');
 		
 	Vx.when({
 		tipoDeMensaje: 'medicionCruda'
@@ -25,24 +27,19 @@ mongodb.MongoClient.connect(uri, function(err, db) {
 	
 	Vx.when({ 
 		tipoDeMensaje: 'usuarioLogin'
-	}, function(login_msg, response){
-		col_usuarios.find({clavePublica:login_msg.clavePublica}).toArray(function(err, usuarios){
+	}, function(login_msg, response){		
+		col_usuarios.find({clavePublica:login_msg.clavePublica}).toArray(function(err, usuarios){			
 			if(usuarios.length>0){
 				var usuario = usuarios[0];
-				response.send({
-					usuarioValido: true,
-					idUsuario: usuario._id,
-					instrumentos: [{   
-							idInstrumento: "ascdvkamoc",
-							descripcion: "Calibre Mitutoyo",
-							precision: 0.05,
-							unidad: "cm"
-						},{   
-							idInstrumento: "cvsdvsdvnd",
-							descripcion: "Micrometro digimess",
-							precision: 0.005,
-							unidad: "mm"
-						}]
+				col_instrumentos.find({idUsuarioOwner: usuario._id.toString()}).toArray(function(err, instrumentos){
+					response.send({
+						usuarioValido: true,
+						apellido: usuario.apellido,
+						nombre: usuario.nombre,
+						nombreUsuario: usuario.nombreUsuario,
+						idUsuario: usuario._id,
+						instrumentos: instrumentos						
+					});
 				});
 			} else{
 				response.send({
@@ -53,33 +50,16 @@ mongodb.MongoClient.connect(uri, function(err, db) {
 	});
 	
 	Vx.when({ 
-		tipoDeMensaje: 'buscarTipoPiezas'
+		tipoDeMensaje: 'buscarTiposDePieza'
 	}, function(busq_piezas, response){
-		col_piezas.find({}).toArray(function(err, piezas){
-			piezas = _.map(piezas, function(pieza){ 
-				pieza["idTipoPieza"] = pieza["_id"];
-        		delete pieza["_id"];
-				return pieza;
+		col_tipos_de_pieza.find({ descripcion: new RegExp(busq_piezas.textoBusqueda)}).toArray(function(err, tipos_de_pieza){
+			tipos_de_pieza = _.map(tipos_de_pieza, function(tipo_de_pieza){ 
+				tipo_de_pieza["idTipoDePieza"] = tipo_de_pieza["_id"];
+        		delete tipo_de_pieza["_id"];
+				return tipo_de_pieza;
 			});
 			response.send({
-				tipoPiezas: piezas
-			});
-		});
-	});
-	
-	
-	Vx.when({ 
-		tipoDeMensaje: 'buscarCotas'
-	}, function(busq_cotas, response){
-		col_cotas.find({idTipoPieza:busq_cotas.idTipoPieza}).toArray(function(err, cotas){
-			cotas = _.map(cotas, function(cota){ 
-				cota["idCota"] = cota["_id"];
-				delete cota["_id"];
-				return cota;
-				
-			});
-			response.send({
-				cotas: cotas
+				tiposDePieza: tipos_de_pieza
 			});
 		});
 	});
